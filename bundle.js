312c1f8a0d259eb8adfc88fa21e0a1db5ada525d
Michal Jána <michal.jana@hotmail.com>
2018-09-13 11:20:18 +0200

    Starting Lodash, Finished old Bootstrap

--- bundle.js
+++ bundle.js
@@ -2,10 +2,7 @@
 var _ = require('lodash');
 var array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(_without(array, 3));
-
-
-
+console.log(_.without(array, 3));
 },{"lodash":2}],2:[function(require,module,exports){
 (function (global){
 /**
--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var _ = require('lodash');
 var array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(_without(array, 3));
-
-
+console.log(_.without(array, 3));
